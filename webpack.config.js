86ea32aff83b478890ff3146b61d7591ded5178b
Stephane P. Pericat <contact@pericat.net>
2019-09-14 14:59:41 -0500

    added custom html template for dev server; added static assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Boilerplate",
+      template: "index.html"
+    })
+  ],
   devServer: {
-    open: true
+    open: true,
+    contentBase: path.join(__dirname, "public")
   }
 };
