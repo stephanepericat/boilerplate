1902628efb4bcce7b2ea131ac229ba84d2a29d7d
Stephane P. Pericat <contact@pericat.net>
2019-09-14 11:03:23 -0500

    jest transform stub

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,10 @@ module.exports = {
     "!src/index.js",
     "!**/node_modules/**"
   ],
-  coverageDirectory: "coverage"
+  coverageDirectory: "coverage",
+  transform: {
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "^.+\\.jsx?$": "babel-jest"
+  }
 };
